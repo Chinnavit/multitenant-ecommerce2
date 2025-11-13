2fb46764cb778bb5031aff5cceaf6dff9611545c
SIN <Chinnawit.S@ku.th>
2025-11-13 10:02:32 +0700

    7:test

--- Desktop/Project Test/test 5/multitenant-ecommerce 2/src/collections/Orders.ts
+++ Desktop/Project Test/test 5/multitenant-ecommerce 2/src/collections/Orders.ts
@@ -3,57 +3,50 @@ import type { CollectionConfig } from "payload";
 import { isSuperAdmin } from "@/lib/access";
 
 export const Orders: CollectionConfig = {
-  slug: "orders",
-  access: {
-    read: ({ req }) => isSuperAdmin(req.user),
-    create: ({ req }) => isSuperAdmin(req.user),
-    update: ({ req }) => isSuperAdmin(req.user),
-    delete: ({ req }) => isSuperAdmin(req.user),
-  },
-  admin: {
-    useAsTitle: "name",
-  },
-  fields: [
-    {
-      name: "name",
-      type: "text",
-      required: true,
+    slug:"orders",
+    access: {
+        read: ({ req }) => isSuperAdmin(req.user),
+        create: ({ req }) => isSuperAdmin(req.user),
+        update: ({ req }) => isSuperAdmin(req.user),
+        delete: ({ req }) => isSuperAdmin(req.user),
     },
-    {
-      name: "user",
-      type: "relationship",
-      relationTo: "users",
-      required: true,
-      hasMany: false,
+    admin: {
+        useAsTitle: "name",
     },
-    {
-      name: "product",
-      type: "relationship",
-      relationTo: "products",
-      required: true,
-      hasMany: false,
-    },
-    {
-      name: "stripeCheckoutSessionId",
-      type: "text",
-      required: true,
-      admin: {
-        description: "Stripe checkout session associated with the order",
-      },
-    },
-    {
-      name: "stripeAccountId",
-      type: "text",
-      admin: {
-        description: "Stripe account associated with the order",
-      },
-    },
-    {
-      name: "configuration",
-      type: "json", // ใช้เก็บ { frameId: "...", matColor: "white", dimensions: { w: 10, h: 8 }, uploadImage: "url..." }
-      admin: {
-        description: "Custom framing details selected by user",
-      },
-    },
-  ],
+    fields: [
+        {
+            name: "name",
+            type: "text",
+            required: true,
+        },
+        {
+            name: "user",
+            type: "relationship",
+            relationTo: "users",
+            required: true,
+            hasMany: false,
+        },
+        {
+            name: "product",
+            type: "relationship",
+            relationTo: "products",
+            required: true,
+            hasMany: false,
+        },
+        {
+            name: "stripeCheckoutSessionId",
+            type: "text",
+            required: true,
+            admin: {
+                description: "Stripe checkout session associated with the order"
+            }
+        },
+        {
+            name: "stripeAccountId",
+            type: "text",
+            admin: {
+                description: "Stripe account associated with the order"
+            }
+        },
+    ],
 };
