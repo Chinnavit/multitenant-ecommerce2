e0ca0c2d40adc8e4e725ea917c73e37a8f91f41b
SIN <Chinnawit.S@ku.th>
2025-11-28 05:21:25 +0700

    26: fix product

--- Desktop/Project Test/test 5/multitenant-ecommerce 2/src/modules/framing/server/procedures.ts
+++
@@ -1,9 +0,0 @@
-import { baseProcedure, createTRPCRouter } from "@/trpc/init";
-
-export const framingRouter = createTRPCRouter({
-  getOptions: baseProcedure.query(async ({ ctx }) => {
-    const frames = await ctx.db.find({ collection: "frames", limit: 100, pagination: false });
-    const mats = await ctx.db.find({ collection: "mats", limit: 100, pagination: false });
-    return { frames: frames.docs, mats: mats.docs };
-  }),
-});
